840a0ff61ba838f207a49fb802a5a96032c833a5
Sekh Gulam Mainuddin <sekhgmainuddin@gmail.com>
2022-12-28 08:33:23 +0530

    POTD

---
+++ Problem Of The Day/Leetcode/Medium/MaximumBagsWithFullCapacityOfRocks.cpp
@@ -0,0 +1,26 @@
+
+// https://leetcode.com/problems/maximum-bags-with-full-capacity-of-rocks/description/
+
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        int cnt = 0;
+        
+        for(int i=0;i<capacity.size();i++) capacity[i] = capacity[i] - rocks[i];
+        
+        sort(begin(capacity),end(capacity));
+        
+        for(auto it : capacity){
+            if(it == 0){ 
+                cnt++;
+                continue;
+            }
+            additionalRocks -= it;
+            if(additionalRocks >=0) cnt++;
+            else break;
+        }
+        
+        return cnt;
+        
+    }
+};
