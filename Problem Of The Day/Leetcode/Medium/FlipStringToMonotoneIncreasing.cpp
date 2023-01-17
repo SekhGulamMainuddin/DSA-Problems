61df935254392e9a075ca9711414d21599b4ea52
Sekh Gulam Mainuddin <sekhgmainuddin@gmail.com>
2023-01-17 17:27:35 +0530

    POTD

---
+++ Problem Of The Day/Leetcode/Medium/FlipStringToMonotoneIncreasing.cpp
@@ -0,0 +1,17 @@
+
+// https://leetcode.com/problems/flip-string-to-monotone-increasing/description/
+
+class Solution {
+public:
+    int minFlipsMonoIncr(const std::string& S, int counter_one  = 0, int counter_flip = 0) {
+        for (auto ch : S) {
+            if (ch == '1') {
+                ++counter_one;
+            } else {
+                ++counter_flip;
+            }
+            counter_flip = std::min(counter_one, counter_flip);
+        }
+        return counter_flip;
+    }
+};
