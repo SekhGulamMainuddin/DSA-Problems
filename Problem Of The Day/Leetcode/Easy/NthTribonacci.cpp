6e890e0c2a15ea1786fcacb1784aa8d723eb026a
Sekh Gulam Mainuddin <sekhgmainuddin@gmail.com>
2023-01-30 10:06:42 +0530

    POTD

---
+++ Problem Of The Day/Leetcode/Easy/NthTribonacci.cpp
@@ -0,0 +1,26 @@
+
+// https://leetcode.com/problems/n-th-tribonacci-number/description/
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0)
+            return 0;
+        else if(n==1 || n==2)
+            return 1;
+        int previousThree= 0;
+        int previousTwo= 1;
+        int previousOne= 1;
+        int sum;
+
+        for(int i=3; i<=n; i++){
+            sum= previousThree + previousTwo + previousOne;
+            previousThree= previousTwo;
+            previousTwo= previousOne;
+            previousOne= sum;
+        }
+
+        return previousOne;
+
+    }
+};
