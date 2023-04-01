87537b6f160bd408b21849e1879f466e8dbb996e
Sekh Gulam Mainuddin <sekhgmainuddin@gmail.com>
2023-04-01 16:25:32 +0530

    Reversal Algorithm

--- DSA Busted/Arrays/rotateArray.java
+++ DSA Busted/Arrays/rotateArray.java
@@ -30,3 +30,37 @@ class Solution {
 
     }
 }
+
+https://practice.geeksforgeeks.org/problems/rotate-array-by-n-elements-1587115621/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+
+// Using Reversal Algorithm TC - O(N) SC - O(1)
+// class Solution{
+//     public:
+    
+//     //Function to rotate an array by d elements in counter-clockwise direction. 
+//     void rotateArr(int arr[], int d, int n){
+//         // code here
+//         // Incase rotation steps is more than length of array
+//         d= d%n;
+//         int i= 0, j=d-1;
+//         while(i<j){
+//             swap(arr[i], arr[j]);
+//             i++;
+//             j--;
+//         }
+//         i= d;
+//         j= n-1;
+//         while(i<j){
+//             swap(arr[i], arr[j]);
+//             i++;
+//             j--;
+//         }
+//         i= 0;
+//         j= n-1;
+//         while(i<j){
+//             swap(arr[i], arr[j]);
+//             i++;
+//             j--;
+//         }
+//     }
+// };
